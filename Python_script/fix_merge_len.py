b878dbc1e19c1170524b3b58aad7dbec08f7e646
ucabuk <36818676+ucabuk@users.noreply.github.com>
2025-01-07 17:05:38 +0100

    Update fix_merge_len.py
    added glob library to find file with pattern.

--- Python_script/fix_merge_len.py
+++ Python_script/fix_merge_len.py
@@ -1,16 +1,27 @@
 # Author: Ugur Cabuk
 #!/bin/python3
+import glob
 
-#Extract the second column from the len_samples file
+len_files = glob.glob("*.len")
+if not len_files:
+    raise FileNotFoundError("No file ending with .len found.")
+len_file = len_files[0]
+
+#Extract the second column from the len_file
 gene_lengths = []
-with open("len_samples", 'r') as file:
+with open(len_file, 'r') as file:
     next(file)  # Skip the first line
     for line in file:
         gene_length = line.split()[1]
         gene_lengths.append(gene_length)
 
-#"numreads_samples.txt", append gene lengths to each line
-with open("numreads_samples", 'r') as input_file, open("numreads_sample_fixed.tsv", 'w') as output_file:
+numreads_samples_files = glob.glob("*_raw_quant.sf")
+if not numreads_samples_files:
+    raise FileNotFoundError("No file ending with _raw_quant.sf found.")
+
+numreads_samples_file = numreads_samples_files[0]
+
+with open(numreads_samples_file, 'r') as input_file, open("numreads_sample_fixed.tsv", 'w') as output_file:
     header = input_file.readline().strip().split('\t') 
 
     #gene_length to the header
