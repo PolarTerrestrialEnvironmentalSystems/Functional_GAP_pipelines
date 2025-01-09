ceaa665d59eec44e4715ec4c56c582a09f0ff250
ucabuk <36818676+ucabuk@users.noreply.github.com>
2025-01-09 22:04:12 +0100

    Update KOs_to_pathway.R
    improved the script
    + added metadata.tsv lines

--- R_script/KOs_to_pathway.R
+++ R_script/KOs_to_pathway.R
@@ -46,7 +46,7 @@ kegg_ref_db_2 <- kegg_ref_db[-which(is.na(kegg_ref_db$Gene_name)),]
 
 setwd("/albedo/work/user/ugcabuk/R_analyze/KEGG_diversity")
 
-analyze_kegg_diversity <- function(eggnog_file, gene_abundance_file, output_file) {
+analyze_kegg_diversity <- function(eggnog_file, gene_abundance_file,metadata, output_file) {
 #eggnog input file
 
 eggnog_input <- fread(eggnog_file)
@@ -56,8 +56,17 @@ quant_merge <- fread(gene_abundance_file, header = TRUE)
 colnames(quant_merge)[1] <- "gene_id"
 
 #colnames(quant_merge) <- c("gene_id","18716","17948","6379","9157","11508","13743","22974","21740","20620","19590","3025","16946","53","712","1817","4322","5149","15596","14667","7569","6869","8491","20053","22505","260","1195","2482","17212","19192","21057","9805","12977","10402","11769","14320","15125","16079","18408","10086","13959","16535","13553","gene_length")
-colnames(quant_merge) <- c("gene_id",18715,17948,6400,9190,11536,13746,22982,21753,20630,19593,3023,16946,76,724,1858,4381,5168,15599,14669,7562,6890,8513,20059,22518,264,1212,2504,17222,19192,21068,9816,12992,10428,11791,14020,14315,15123,16084,10091,13930,16541,13588,"gene_length")
+#colnames(quant_merge) <- c("gene_id",18715,17948,6400,9190,11536,13746,22982,21753,20630,19593,3023,16946,76,724,1858,4381,5168,15599,14669,7562,6890,8513,20059,22518,264,1212,2504,17222,19192,21068,9816,12992,10428,11791,14020,14315,15123,16084,10091,13930,16541,13588,"gene_length")
 
+metadata <- fread(metadata)
+rename_map <- setNames(metadata$Age, metadata$Name)
+colnames(quant_merge) <- ifelse(colnames(quant_merge) %in% names(rename_map), rename_map[colnames(quant_merge)], colnames(quant_merge))
+
+age_columns <- intersect(colnames(quant_merge), rename_map)
+age_columns <- age_columns[order(as.numeric(age_columns))]
+
+#change the col name of egnnog
+colnames(eggnog_input)[1] <- "gene_id"
 quant_merge$gene_length <- NULL
 
 
@@ -85,12 +94,19 @@ quant_eggnog_merge_4 <- quant_eggnog_merge_4[-which(quant_eggnog_merge_4$KEGG_ko
 which(is.na(quant_eggnog_merge_4$KEGG_ko))
 
 #wide to long format
+#quant_eggnog_merge_long <- quant_eggnog_merge_4 %>%
+#  pivot_longer(
+#    cols = `18716`:`13553`,
+#    names_to = "Ages",
+#    values_to = "TPM_value"
+#  )
+
 quant_eggnog_merge_long <- quant_eggnog_merge_4 %>%
   pivot_longer(
-    cols = `18716`:`13553`,
+    cols = all_of(age_columns),
     names_to = "Ages",
     values_to = "TPM_value"
-  )
+)
 
 
 #merge kegg reference file with your prepared eggnog annotations
@@ -161,43 +177,40 @@ write.csv(ko_abund, file = paste0(output_file, ".csv"))
 # Normalized_Gene_Count
 # PROKGAP
 prokgap <- analyze_kegg_diversity("non_redundant_PROKGAP_protein_eggNOG.emapper.annotations",
-                       "prokGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv",
+                       "prokGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv","metadata.tsv"
                        "prokgap_KO_pathway_CPM_eggnog_abundance")
 # EUKGAP
 eukgap <- analyze_kegg_diversity("non_redundant_EUKGAP_protein_eggNOG.emapper.annotations",
-                       "eukGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv",
+                       "eukGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv","metadata.tsv"
                        "eukgap_KO_pathway_CPM_eggnog_abundance")
 
 # PRECLASS_PROKGAP
 preclass_prok <- analyze_kegg_diversity("non_redundant_preclass_prokGAP_protein_eggNOG.emapper.annotations",
-                       "preclass_prokGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv",
+                       "preclass_prokGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv","metadata.tsv"
                        "preclass_prokgap_KO_pathway_CPM_eggnog_abundance")
 
 # PRECLASS_EUKGAP
 preclass_euk <- analyze_kegg_diversity("non_redundant_preclass_eukGAP_protein_eggNOG.emapper.annotations",
-                       "preclass_eukGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv",
+                       "preclass_eukGAP_all_lake_lama_gene_quant.CPM.6m.fixed.tsv","metadata.tsv"
                        "preclass_eukgap_KO_pathway_CPM_eggnog_abundance")
 
 
-
-
 # RAW COUNT
-
 # PROKGAP
 prokgap <- analyze_kegg_diversity("non_redundant_PROKGAP_protein_eggNOG.emapper.annotations",
-                       "prokGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv",
+                       "prokGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv","metadata.tsv"
                        ""prokgap_KO_pathway_raw_eggnog_abundance"")
 # EUKGAP
 eukgap <- analyze_kegg_diversity("non_redundant_EUKGAP_protein_eggNOG.emapper.annotations",
-                       "eukGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv",
+                       "eukGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv","metadata.tsv"
                        "eukgap_KO_pathway_raw_eggnog_abundance")
 
 # PRECLASS_PROKGAP
 preclass_euk <- analyze_kegg_diversity("non_redundant_preclass_prokGAP_protein_eggNOG.emapper.annotations",
-                       "preclass_prokGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv",
+                       "preclass_prokGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv","metadata.tsv"
                        "preclass_prokgap_KO_pathway_raw_eggnog_abundance")
 
 # PRECLASS_EUKGAP
 preclass_prok <- analyze_kegg_diversity("non_redundant_preclass_eukGAP_protein_eggNOG.emapper.annotations",
-                       "preclass_eukGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv",
+                       "preclass_eukGAP_all_lake_lama_gene_quant.raw_count.fixed.tsv","metadata.tsv"
                        "preclass_eukgap_KO_pathway_raw_eggnog_abundance")
